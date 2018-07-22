025f1a602e7e60ec2fb4cb6300ed559444c8699c
niravt <niravt@meditab.com>
2018-07-22 14:14:35 +0530

    - Kotlin support added. - minor improvements

---
+++ app/src/main/java/com/an/biometric/sample/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.an.biometric.sample
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+import com.niravtukadiya.compat.biometric.BiometricCallback
+import com.niravtukadiya.compat.biometric.BiometricCompat
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), BiometricCallback {
+
+    private var button: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_authenticate.setOnClickListener {
+            BiometricCompat.BiometricBuilder(this)
+                    .setTitle(getString(R.string.biometric_title))
+                    .setSubtitle(getString(R.string.biometric_subtitle))
+                    .setDescription(getString(R.string.biometric_description))
+                    .setNegativeButtonText(getString(R.string.biometric_negative_button_text))
+                    .build()
+                    .authenticate(this)
+        }
+    }
+
+
+    override fun onSdkVersionNotSupported() {
+        Toast.makeText(applicationContext, getString(R.string.biometric_error_sdk_not_supported), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBiometricAuthenticationNotSupported() {
+        Toast.makeText(applicationContext, getString(R.string.biometric_error_hardware_not_supported), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBiometricAuthenticationNotAvailable() {
+        Toast.makeText(applicationContext, getString(R.string.biometric_error_fingerprint_not_available), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBiometricAuthenticationPermissionNotGranted() {
+        Toast.makeText(applicationContext, getString(R.string.biometric_error_permission_not_granted), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBiometricAuthenticationInternalError(error: String?) {
+        Toast.makeText(applicationContext, error, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAuthenticationFailed() {
+        //        Toast.makeText(getApplicationContext(), getString(R.string.biometric_failure), Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationCancelled() {
+        Toast.makeText(applicationContext, getString(R.string.biometric_cancelled), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAuthenticationSuccessful() {
+        Toast.makeText(applicationContext, getString(R.string.biometric_success), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+        //        Toast.makeText(getApplicationContext(), helpString, Toast.LENGTH_LONG).show();
+    }
+
+    override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+        //        Toast.makeText(getApplicationContext(), errString, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ biometric-auth/src/main/java/com/niravtukadiya/compat/biometric/BiometricCallback.kt
@@ -0,0 +1,25 @@
+package com.niravtukadiya.compat.biometric
+
+interface BiometricCallback {
+
+    fun onSdkVersionNotSupported()
+
+    fun onBiometricAuthenticationNotSupported()
+
+    fun onBiometricAuthenticationNotAvailable()
+
+    fun onBiometricAuthenticationPermissionNotGranted()
+
+    fun onBiometricAuthenticationInternalError(error: String?)
+
+
+    fun onAuthenticationFailed()
+
+    fun onAuthenticationCancelled()
+
+    fun onAuthenticationSuccessful()
+
+    fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?)
+
+    fun onAuthenticationError(errorCode: Int, errString: CharSequence?)
+}
